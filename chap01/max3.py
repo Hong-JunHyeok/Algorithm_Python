1f8cfda31aa76e00bca6601e254ea3279913005f
HongJunHyeok <edb1631@naver.com>
2021-09-27 14:21:54 +0900

    ADD: max3.py

---
+++ chap01/max3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+print('세 정수의 최대값을 구합니다.')
+a = int(input('정수 a의 값을 입력하세요...'))
+b = int(input('정수 b의 값을 입력하세요...'))
+c = int(input('정수 c의 값을 입력하세요...'))
+
+maximum = a
+if b > maximum: maximum = b
+if c > maximum: maximum = c
+
+print(f"최대값은 {maximum}입니다")
