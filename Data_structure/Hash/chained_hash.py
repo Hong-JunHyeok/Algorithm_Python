8bd315eb07183acec833acf6ecc579cb5a61e67e
HongJunHyeok <edb1631@naver.com>
2021-10-03 12:12:21 +0900

    ADD: Chained Hash test code

--- Data_structure/Hash/chained_hash.py
+++ Data_structure/Hash/chained_hash.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 from typing import Any, Type
 import hashlib
+from enum import Enum
 
 
 class Node:
@@ -81,3 +82,46 @@ class ChainedHash:
                 print(f'   ->{p.key} ({p.value})', end='')
                 p = p.next
             print()
+
+
+if __name__ == '__main__':
+    Menu = Enum('Menu', ['추가', '삭제', '검색', '덤프', '종료'])  # 메뉴를 선언
+
+    def select_menu() -> Menu:
+        """메뉴 선택"""
+        s = [f'({m.value}){m.name}' for m in Menu]
+        while True:
+            print(*s, sep='   ', end='')
+            n = int(input(': '))
+            if 1 <= n <= len(Menu):
+                return Menu(n)
+
+    hash = ChainedHash(13)  # 크기가 13인 해시 테이블을 생성
+
+    while True:
+        menu = select_menu()  # 메뉴 선택
+
+        if menu == Menu.추가:
+            key = int(input('추가할 키를 입력하세요 : '))
+            val = input('추가할 값을 입력하세요')
+            if not hash.add(key, val):
+                print('추가를 실패했습니다.')
+
+        elif menu == Menu.삭제:
+            key = int(input('삭제할 키를 입력하세요 : '))
+            if not hash.remove(key):
+                print('삭제를 실패했습니다.')
+
+        elif menu == Menu.검색:
+            key = int(input('검색할 키를 입력하세요 : '))
+            t = hash.search(key)
+
+            if t is None:
+                print('검색할 데이터가 없습니다.')
+            else:
+                print(f'검색한 키를 갖는 값은 {t}입니다.')
+        elif menu == Menu.덤프:
+            hash.dump()
+
+        else:
+            break
