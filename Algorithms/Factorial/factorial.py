c7238b24970e28f17be66fae6c4c0aab7966dabd
HongJunHyeok <edb1631@naver.com>
2021-10-03 21:07:34 +0900

    ADD: Factorial Algorithm

---
+++ Algorithms/Factorial/factorial.py
@@ -0,0 +1,10 @@
+def factorial(n: int) -> int:
+    if n > 0:
+        return n * factorial(n - 1)
+    else:
+        return 1
+
+
+if __name__ == '__main__':
+    n = int(input('출력할 팩토리얼 값을 입력하세요 : '))
+    print(f'{n}의 팩토리얼은 {factorial(n)}입니다.')
